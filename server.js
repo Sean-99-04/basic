ae69bad21ffeeccdffa6631b3008c08fa595946f
Sean Stocker <s.stocks99@gmail.com>
2020-12-28 00:39:23 +0000

    Added routes folder and basic mongoose connection

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const router = require('./routes/routes.js')
+
+const uri = 'mongodb+srv://Sean:m0ngoPass@cluster0.xuroh.mongodb.net/pets?retryWrites=true&w=majority'
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.engine('.hbs', exphbs({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+app.use('/routes', router)
+
 app.get('/', (req, res) => {
-    res.render('home', {})
+    const name = 'Sean'
+    res.render('home', {name})
+})
+
+app.get('/*', (req, res) => {
+    res.send('404 There is nothing here')
 })
 
 app.listen(process.env.PORT || 5000,
