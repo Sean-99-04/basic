5f18c3abb098e3311adc8097b54db06486e3e26d
Sean Stocker <s.stocks99@gmail.com>
2021-01-11 16:27:04 +0000

    reverted more changes

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-if ((process.env.NODE_ENV = "development")) {
-  require("dotenv").config();
-}
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
